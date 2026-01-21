eaecfd6b99b9f2cc7f81f37a6737b64e7b5c03a3
akarshvn <akarsh.velan@gmail.com>
2026-01-21 21:54:15 +0530

    Update Controller Services to work with React

--- src/main/java/com/hostelassist/notices/NoticeController.java
+++ src/main/java/com/hostelassist/notices/NoticeController.java
@@ -3,6 +3,7 @@ package com.hostelassist.notices;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/notices")
 
--- src/main/java/com/hostelassist/resources/ResourceController.java
+++ src/main/java/com/hostelassist/resources/ResourceController.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/resources")
 public class ResourceController {
